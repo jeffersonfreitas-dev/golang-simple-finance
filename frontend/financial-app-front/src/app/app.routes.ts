0830a9a69f2d800443624602beea89abf732008a
jeffersonfreitas-dev <jefferson.dev21@gmail.com>
2026-02-21 19:08:34 -0300

    add page spa home

--- frontend/financial-app-front/src/app/app.routes.ts
+++ frontend/financial-app-front/src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {
+        path: '',
+        component: AppComponent // Carrega o AppComponent diretamente na raiz
+    },
+    {
         path: 'login',
         loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
-    }
+    },
+    // {
+    //     path: 'register',
+    //     loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent)
+    // }
 ];
